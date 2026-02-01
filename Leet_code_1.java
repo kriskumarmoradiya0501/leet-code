3dda51682e7be450a391cffb3e9f4553beca7b81
kriskumarmoradiya0501 <140172827+kriskumarmoradiya0501@users.noreply.github.com>
2026-02-01 15:18:17 +0530

    Add multiple LeetCode solutions

---
+++ Leet_code_1.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Leet_code_1 {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for(int i=0; i<nums.length ; i++){
+            int complement = target - nums[i];
+            if(map.containsKey(complement)){
+                return new int[]{map.get(complement),i};
+            }
+            else{
+                map.put(nums[i],i);
+            }
+        }
+        throw new IllegalArgumentException("no found");
+    }
+}
