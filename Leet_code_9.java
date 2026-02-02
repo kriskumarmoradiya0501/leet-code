b809a6e7c712bffdd9dc99d6277d6d212a0bf019
kriskumarmoradiya0501 <140172827+kriskumarmoradiya0501@users.noreply.github.com>
2026-02-02 20:23:35 +0530

    Add/update workspace files

---
+++ Leet_code_9.java
@@ -0,0 +1,24 @@
+public class Leet_code_9 {
+
+    private static boolean checkPalindrome(String num) {
+        int left = 0;
+        int right = num.length() - 1;
+
+        while (left < right) {
+            if (num.charAt(left) != num.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        String num = String.valueOf(x);
+        return checkPalindrome(num);
+    }
+}
