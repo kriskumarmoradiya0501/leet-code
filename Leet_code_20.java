3dd0b20623a5ad1c4815f20cecbdaf8733dc0b3e
kriskumarmoradiya0501 <140172827+kriskumarmoradiya0501@users.noreply.github.com>
2026-02-05 21:02:12 +0530

    Add solution for LeetCode problem 20

---
+++ Leet_code_20.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+
+public class Leet_code_20 {
+    public boolean isValid(String s){
+        Stack<Character> stack = new Stack<>();
+        for(int i =0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch == '('||ch== '['||ch=='{'){
+                stack.push(ch);
+            }
+            else if(!stack.isEmpty() && ch == ')' && stack.peek() == '('){
+                stack.pop();
+            }
+            else if(!stack.isEmpty() && ch == ']' && stack.peek() == '['){
+                stack.pop();
+            }
+            else if(!stack.isEmpty()&&ch=='}' && stack.peek() == '{'){
+                stack.pop();
+            }
+            else{
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
