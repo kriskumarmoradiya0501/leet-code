d6a87ca9b24b9b3c13ec64ee2110bfd829f09f67
kriskumarmoradiya0501 <140172827+kriskumarmoradiya0501@users.noreply.github.com>
2026-02-09 18:01:28 +0530

    Add Leet_code_83.java

---
+++ Leet_code_83.java
@@ -0,0 +1,17 @@
+import java.lang.classfile.components.ClassPrinter.ListNode;
+
+public class Leet_code_83 {
+    public ListNode deleteDuplicates(ListNode head){
+        ListNode current = head;
+
+        while (current != null && current.next !=null){
+            if(current.val == current.next.val){
+                current.next = current.next.next;
+            }
+            else{
+                current = current.next;
+            }
+        }
+        return head;
+    }
+}
